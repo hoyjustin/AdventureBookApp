6e88746f0073a14463f35c574c45307f3d2a7cc8
MinhalSyed <minhal@ualberta.ca>
2013-10-27 13:27:44 -0600

    Added an EditOption XML in the res/layout/ folder

--- AdventureBook/src/com/example/adventurebook/EditOptionActivity.java
+++ AdventureBook/src/com/example/adventurebook/EditOptionActivity.java
@@ -1,30 +1,9 @@
 package com.example.adventurebook;
 
+import android.app.Activity;
+/*
+ * Creator: Minhal Syed*/
 
-public class EditOptionActivity {
-
-	/**
-	 * @uml.property  name="sController"
-	 * @uml.associationEnd  multiplicity="(1 1)" inverse="editOptionActivity:com.example.adventurebook.SController"
-	 */
-	private SController sController = new com.example.adventurebook.SController();
-
-	/**
-	 * Getter of the property <tt>sController</tt>
-	 * @return  Returns the sController.
-	 * @uml.property  name="sController"
-	 */
-	public SController getSController() {
-		return sController;
-	}
-
-	/**
-	 * Setter of the property <tt>sController</tt>
-	 * @param sController  The sController to set.
-	 * @uml.property  name="sController"
-	 */
-	public void setSController(SController sController) {
-		this.sController = sController;
-	}
-
+public class EditOptionActivity extends Activity{
+	
 }
