32acb7f4422192bc0ad391953e81ce3b83542887
tyleung <tyleung@ualberta.ca>
2013-10-27 18:29:25 -0600

    added create new story ui

--- AdventureBook/gen/com/example/adventurebook/R.java
+++ AdventureBook/gen/com/example/adventurebook/R.java
@@ -25,25 +25,36 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int GotoPageTV=0x7f080002;
-        public static final int SaveOptionBtn=0x7f080005;
-        public static final int Stories=0x7f080006;
-        public static final int action_settings=0x7f08000b;
-        public static final int createNewStoryBtn=0x7f080008;
-        public static final int editText1=0x7f080000;
-        public static final int listView1=0x7f08000a;
-        public static final int list_of_goto_pages=0x7f080004;
-        public static final int myStorylistView=0x7f080007;
-        public static final int onlineLibraryBtn=0x7f080009;
-        public static final int textView1=0x7f080003;
-        public static final int textView2=0x7f080001;
+        public static final int GotoPageTV=0x7f08000c;
+        public static final int SaveOptionBtn=0x7f08000f;
+        public static final int Stories=0x7f080010;
+        public static final int action_settings=0x7f080015;
+        public static final int authorText=0x7f080007;
+        public static final int createNewStoryBtn=0x7f080012;
+        public static final int createStoryButton=0x7f080009;
+        public static final int dateText=0x7f080008;
+        public static final int editStoryDescription=0x7f080006;
+        public static final int editStoryTitle=0x7f080002;
+        public static final int editText1=0x7f08000a;
+        public static final int imageView1=0x7f080003;
+        public static final int listView1=0x7f080014;
+        public static final int list_of_goto_pages=0x7f08000e;
+        public static final int myStorylistView=0x7f080011;
+        public static final int onlineLibraryBtn=0x7f080013;
+        public static final int scrollView1=0x7f080001;
+        public static final int scrollView2=0x7f080005;
+        public static final int storyDescription=0x7f080004;
+        public static final int textView1=0x7f08000d;
+        public static final int textView2=0x7f08000b;
+        public static final int title=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int edit_option=0x7f030001;
-        public static final int list_row=0x7f030002;
-        public static final int my_local_stories=0x7f030003;
-        public static final int online_store=0x7f030004;
+        public static final int create_story=0x7f030001;
+        public static final int edit_option=0x7f030002;
+        public static final int list_row=0x7f030003;
+        public static final int my_local_stories=0x7f030004;
+        public static final int online_store=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -52,6 +63,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int story_description=0x7f050004;
+        public static final int title=0x7f050003;
     }
     public static final class style {
         /** 
--- AdventureBook/src/ca/ualberta/c301/adventurebook/CreateStoryActivity.java
+++ AdventureBook/src/ca/ualberta/c301/adventurebook/CreateStoryActivity.java
@@ -1,54 +1,62 @@
+/*
+ * Copyright (C) <2013>  <Terence Yin Kiu Leung>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.ualberta.c301.adventurebook;
 
+import android.app.Activity;
+import android.os.Bundle;
 
-public class CreateStoryActivity {
+import com.example.adventurebook.R;
 
-	/**
-	 * @uml.property  name="sController"
-	 * @uml.associationEnd  multiplicity="(1 1)" inverse="createStoryActivity:com.example.adventurebook.SController"
-	 */
-	private SController sController = new ca.ualberta.c301.adventurebook.SController();
 
-	/**
-	 * Getter of the property <tt>sController</tt>
-	 * @return  Returns the sController.
-	 * @uml.property  name="sController"
-	 */
-	public SController getSController() {
-		return sController;
+public class CreateStoryActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.create_story);
+
 	}
 
-	/**
-	 * Setter of the property <tt>sController</tt>
-	 * @param sController  The sController to set.
-	 * @uml.property  name="sController"
-	 */
-	public void setSController(SController sController) {
-		this.sController = sController;
-	}
-
-	/** 
-	 * @uml.property name="sController1"
-	 * @uml.associationEnd multiplicity="(1 1)" inverse="createStoryActivity1:com.example.adventurebook.SController"
-	 */
-	private SController sController1 = new ca.ualberta.c301.adventurebook.SController();
-
-	/** 
-	 * Getter of the property <tt>sController1</tt>
-	 * @return  Returns the sController1.
-	 * @uml.property  name="sController1"
-	 */
-	public SController getSController1() {
-		return sController1;
-	}
-
-	/** 
-	 * Setter of the property <tt>sController1</tt>
-	 * @param sController1  The sController1 to set.
-	 * @uml.property  name="sController1"
-	 */
-	public void setSController1(SController sController1) {
-		this.sController1 = sController1;
-	}
-
+	
+//
+//	/**
+//	 * @uml.property  name="sController"
+//	 * @uml.associationEnd  multiplicity="(1 1)" inverse="createStoryActivity:com.example.adventurebook.SController"
+//	 */
+//	private SController sController = new ca.ualberta.c301.adventurebook.SController();
+//
+//	/**
+//	 * Getter of the property <tt>sController</tt>
+//	 * @return  Returns the sController.
+//	 * @uml.property  name="sController"
+//	 */
+//	public SController getSController() {
+//		return sController;
+//	}
+//
+//	/**
+//	 * Setter of the property <tt>sController</tt>
+//	 * @param sController  The sController to set.
+//	 * @uml.property  name="sController"
+//	 */
+//	public void setSController(SController sController) {
+//		this.sController = sController;
+//	}
+	
 }
