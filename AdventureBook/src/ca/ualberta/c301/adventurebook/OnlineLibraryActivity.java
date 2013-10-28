961f1bcdc85f93de9b3a84308d0776e15da9d2d1
MinhalSyed <minhal@ualberta.ca>
2013-10-27 22:48:10 -0600

    Added an online_library xml. Not finished tho.

--- AdventureBook/src/ca/ualberta/c301/adventurebook/OnlineLibraryActivity.java
+++ AdventureBook/src/ca/ualberta/c301/adventurebook/OnlineLibraryActivity.java
@@ -1,33 +1,84 @@
 package ca.ualberta.c301.adventurebook;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
 
-/**
- * @uml.dependency   supplier="com.example.adventurebook.SController"
- */
-public class OnlineLibraryActivity {
 
-	/** 
-	 * @uml.property name="sController"
-	 * @uml.associationEnd multiplicity="(1 1)" inverse="onlineLibraryActivity:com.example.adventurebook.SController"
-	 */
-	private SController sController = new ca.ualberta.c301.adventurebook.SController();
+import com.example.adventurebook.R;
 
-	/** 
-	 * Getter of the property <tt>sController</tt>
-	 * @return  Returns the sController.
-	 * @uml.property  name="sController"
-	 */
-	public SController getSController() {
-		return sController;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class OnlineLibraryActivity extends Activity{
+
+	ArrayList<dummy_story_class> dummyStories;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.online_library);
+		createFakeData();
+		populateListView();
+	}
+	
+	private void createFakeData() {
+		//Dummy Data:
+		
+		dummyStories = new ArrayList<dummy_story_class>();
+		dummyStories.add(new dummy_story_class("story1","author1","date1",R.drawable.fish));
+		dummyStories.add(new dummy_story_class("story2","author2","date2",R.drawable.help));	
+	}
+	private void populateListView(){
+		
+		ListView onlineLV = (ListView) findViewById(R.id.online_library_listView);
+		ArrayAdapter<dummy_story_class> adapter = new CustomAdapter();
+		onlineLV.setAdapter(adapter);
+	}
+	private class CustomAdapter extends ArrayAdapter<dummy_story_class>{
+
+		public CustomAdapter() {
+			super(OnlineLibraryActivity.this, R.layout.online_story_list_row, dummyStories);
+			// TODO Auto-generated constructor stub
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			//Tutorial used from: https://www.youtube.com/watch?v=WRANgDgM2Zg
+			
+			// Make sure we have a view to work with (may have been given null)
+			
+			View itemView = convertView;
+			if (itemView == null) {
+				itemView = getLayoutInflater().inflate(R.layout.online_story_list_row, parent, false);
+			}
+			
+			dummy_story_class currentStory = dummyStories.get(position);
+			
+			// Fill the view
+			ImageView imageView = (ImageView)itemView.findViewById(R.id.storyImageView);
+			imageView.setImageResource(currentStory.getImageIcon());
+			
+			TextView authorText = (TextView) itemView.findViewById(R.id.authorTV);
+			authorText.setText(currentStory.getAuthor());
+
+			TextView dateText = (TextView) itemView.findViewById(R.id.dateCreatedTv);
+			dateText.setText(currentStory.getDate());
+			
+			return itemView;
+		}	
 	}
 
-	/** 
-	 * Setter of the property <tt>sController</tt>
-	 * @param sController  The sController to set.
-	 * @uml.property  name="sController"
-	 */
-	public void setSController(SController sController) {
-		this.sController = sController;
-	}
-
+	
 }
