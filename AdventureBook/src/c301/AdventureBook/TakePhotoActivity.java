4ad070e08f13341c28a38c29c4fbfa0c015f8d22
zhang43676 <zz9@ualberta.ca>
2013-10-30 13:40:05 +0800

    select photo from path

--- AdventureBook/src/c301/AdventureBook/TakePhotoActivity.java
+++ AdventureBook/src/c301/AdventureBook/TakePhotoActivity.java
@@ -3,16 +3,20 @@ package c301.AdventureBook;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 
 import com.example.adventurebook.R;
-
 public class TakePhotoActivity extends Activity{
-	int REQUEST_CODE = 0;
-	
+	//int REQUEST_CODE = 0;
+	private static final int SELECT_PHOTO = 100;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -24,7 +28,6 @@ public class TakePhotoActivity extends Activity{
 		uploadFromPhone.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
-				final int SELECT_PHOTO = 100;
 				Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
 				photoPickerIntent.setType("image/*");
 				startActivityForResult(photoPickerIntent, SELECT_PHOTO);
@@ -42,5 +45,29 @@ public class TakePhotoActivity extends Activity{
 		
 		
 			
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnIntent) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, imageReturnIntent);
+		switch(requestCode){
+		case SELECT_PHOTO:
+			If(resultCode == RESULT_OK);{
+				Uri selectedImage = imageReturnIntent.getData();
+				String[] filePathColumn = {MediaStore.Images.Media.DATA};
+				Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+				cursor.moveToFirst();
+				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+				String filePath = cursor.getString(columnIndex);
+				cursor.close();
+				Bitmap yourSelcetedImage = BitmapFactory.decodeFile(filePath);
+				
+			}
+			
+		}
+	}
+	private void If(boolean b) {
+		// TODO Auto-generated method stub
+		
 	}
 }
