53dfa373b63730da522351245e6b2f8756bc8139
Minhal Syed <minhal@ualberta.ca>
2013-10-29 18:22:28 -0600

    dsadsa

--- AdventureBook/src/c301/AdventureBook/EditStory/CreateStoryActivity.java
+++ AdventureBook/src/c301/AdventureBook/EditStory/CreateStoryActivity.java
@@ -69,29 +69,5 @@ public class CreateStoryActivity extends Activity{
 		mDate.setText("Date: " + formattedDate);
 	}
 	
-//
-//	/**
-//	 * @uml.property  name="sController"
-//	 * @uml.associationEnd  multiplicity="(1 1)" inverse="createStoryActivity:com.example.adventurebook.SController"
-//	 */
-//	private SController sController = new ca.ualberta.c301.adventurebook.SController();
-//
-//	/**
-//	 * Getter of the property <tt>sController</tt>
-//	 * @return  Returns the sController.
-//	 * @uml.property  name="sController"
-//	 */
-//	public SController getSController() {
-//		return sController;
-//	}
-//
-//	/**
-//	 * Setter of the property <tt>sController</tt>
-//	 * @param sController  The sController to set.
-//	 * @uml.property  name="sController"
-//	 */
-//	public void setSController(SController sController) {
-//		this.sController = sController;
-//	}
 	
 }
