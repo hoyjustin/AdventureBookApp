3fcd848f1e378b287240d76cab04d667d79cd781
tyleung <tyleung@ualberta.ca>
2013-10-29 19:10:16 -0600

    finished annotations xml

--- AdventureBook/src/ca/ualberta/c301/adventurebook/AnnotationActivity.java
+++ AdventureBook/src/ca/ualberta/c301/adventurebook/AnnotationActivity.java
@@ -1,30 +1,44 @@
 package ca.ualberta.c301.adventurebook;
 
+import android.app.Activity;
+import android.os.Bundle;
 
-public class AnnotationActivity {
+import com.example.adventurebook.R;
 
-	/**
-	 * @uml.property  name="sController"
-	 * @uml.associationEnd  multiplicity="(1 1)" inverse="annotationActivity:com.example.adventurebook.SController"
-	 */
-	private SController sController = new ca.ualberta.c301.adventurebook.SController();
 
-	/**
-	 * Getter of the property <tt>sController</tt>
-	 * @return  Returns the sController.
-	 * @uml.property  name="sController"
-	 */
-	public SController getSController() {
-		return sController;
+public class AnnotationActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.annotations);
 	}
-
-	/**
-	 * Setter of the property <tt>sController</tt>
-	 * @param sController  The sController to set.
-	 * @uml.property  name="sController"
-	 */
-	public void setSController(SController sController) {
-		this.sController = sController;
-	}
+	
+	
+	
+//	/**
+//	 * @uml.property  name="sController"
+//	 * @uml.associationEnd  multiplicity="(1 1)" inverse="annotationActivity:com.example.adventurebook.SController"
+//	 */
+//	private SController sController = new ca.ualberta.c301.adventurebook.SController();
+//
+//	/**
+//	 * Getter of the property <tt>sController</tt>
+//	 * @return  Returns the sController.
+//	 * @uml.property  name="sController"
+//	 */
+//	public SController getSController() {
+//		return sController;
+//	}
+//
+//	/**
+//	 * Setter of the property <tt>sController</tt>
+//	 * @param sController  The sController to set.
+//	 * @uml.property  name="sController"
+//	 */
+//	public void setSController(SController sController) {
+//		this.sController = sController;
+//	}
 
 }
