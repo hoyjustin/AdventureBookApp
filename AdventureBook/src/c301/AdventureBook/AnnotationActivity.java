6d63c164e2fd94a871ba3c239420187f77840db9
ltong2 <ltong2@ualberta.ca>
2013-10-31 12:24:27 -0600

    test my github

--- AdventureBook/src/c301/AdventureBook/AnnotationActivity.java
+++ AdventureBook/src/c301/AdventureBook/AnnotationActivity.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.adventurebook.R;
-import com.example.projectpart3.Annotations;
+
 
 
 public class AnnotationActivity extends Activity {
