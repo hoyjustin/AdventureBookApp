22fe0b4e4bd58eea49d858db2fbcd0ec8a4e8759
MinhalSyed <minhal@ualberta.ca>
2013-11-03 22:04:43 -0700

    added javadoc for Story class

--- AdventureBook/src/c301/AdventureBook/Models/Story.java
+++ AdventureBook/src/c301/AdventureBook/Models/Story.java
@@ -6,9 +6,15 @@ import java.util.List;
 
 import com.example.adventurebook.R;
 
-
+/**
+ * This is a Story Model Class. A Story is an object that contains title,  description, 
+ * author, date, thumbnail, and list of Page objects. 
+ * 
+ * @author Minhal Syed
+ * @author Justin Hoy
+ *
+ */
 public class Story implements Serializable{
-	//This class was just created for testing the onlineStoryActivity.
 	
 	private String title;
 	private String description;
@@ -17,15 +23,26 @@ public class Story implements Serializable{
 	private String imagePath;
 	public List<Page> pages = new ArrayList<Page>();
 	
+	
+	/**
+	 * This is the constructor function for the Story class.
+	 * It creates a new Story object with information provided.  
+	 * @param title title of the Story.
+	 * @param description description of the Story.
+	 * @param author author of the Story.
+	 * @param date date on which the Story was first created.
+	 * @param imagePath path of the thumnail picture of the Story.
+	 */
 	public Story(String title, 	String description, String author, String date, String imagePath){
 		this.title = title;
 		this.description = description;
 		this.author = author;
 		this.date = date;
-		this.imagePath = imagePath;//R.drawable.fish;
+		this.imagePath = imagePath;
 	}
 	
 	/**
+	 * This function returns the description of the story.
 	 * @return the description
 	 */
 	public String getDescription() {
@@ -33,6 +50,7 @@ public class Story implements Serializable{
 	}
 
 	/**
+	 * This function sets the description of the story.
 	 * @param description the description to set
 	 */
 	public void setDescription(String description) {
@@ -40,48 +58,84 @@ public class Story implements Serializable{
 	}
 
 	/**
-	 * @param pages the pages to set
+	 * This function returns the created date of the story.
+	 * @return date
 	 */
-	public void setPages(List<Page> pages) {
-		this.pages = pages;
-	}
-
 	public String getDate() {
 		return date;
 	}
-
+	
+	/**
+	 * This function sets the creation date of the story.
+	 * @param date
+	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
-
+	/**
+	 * This function returns the title of the story.
+	 * @return title
+	 */
 	public String getTitle() {
 		return title;
 	}
+	/**
+	 * This function sets the title of the story.
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	/**
+	 * This function returns the author of the story.
+	 * @return String author
+	 */
 	public String getAuthor() {
 		return author;
 	}
+	/**
+	 * This function sets the author of the story.
+	 * @param author
+	 */
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-
-
-
+	
+	/**
+	 * This function returns the imagePath of the thumbnail photo of the Story.
+	 * @return
+	 */
 	public String getImagePath() {
 		return imagePath;
 	}
-
+	/**
+	 * This function sets the imagePath of the thumbnail photo of the story.
+	 * @param imagePath
+	 */
 	public void setImagePath(String imagePath) {
 		this.imagePath = imagePath;
 	}
 
+	/**
+	 * This function returns the pages of the story.
+	 * @return
+	 */
 	public List<Page> getPages() {
 		return this.pages;
 	}
-	public void addPage(Page x) {
-		this.pages.add(x);
+	/**
+	 * This function adds a page to the pages list.
+	 * @param page
+	 */
+	public void addPage(Page page) {
+		this.pages.add(page);
+	}
+	/**
+	 * This function sets all the pages of a story.
+	 * @param pages the pages to set
+	 */
+	public void setPages(List<Page> pages) {
+		this.pages = pages;
 	}
 
 }
