fc139b26745a0901f5905006fafedaecca309388
ltong2 <ltong2@ualberta.ca>
2013-11-04 21:39:45 -0500

    spalsh screen

---
+++ AdventureBook/src/c301/AdventureBook/coverimage.java
@@ -0,0 +1,18 @@
+package c301.AdventureBook;
+
+
+import com.example.adventurebook.R;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class coverimage extends Activity{
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.coverend);			
+			}
+}
