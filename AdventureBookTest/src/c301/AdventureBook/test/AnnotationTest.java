222cda613b12130b4e753503e16600fad359cce5
tyleung <tyleung@ualberta.ca>
2013-11-08 15:09:55 -0700

    added to annotation test

--- AdventureBookTest/src/c301/AdventureBook/test/AnnotationTest.java
+++ AdventureBookTest/src/c301/AdventureBook/test/AnnotationTest.java
@@ -1,5 +1,7 @@
 package c301.AdventureBook.test;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,23 +17,63 @@ import c301.AdventureBook.Models.Annotation;
 public class AnnotationTest extends AndroidTestCase
 {
 	private Annotation annotation;
+	private String author = "Some author";
+	private String comment = "Some comment";
+	private String imagepath1 = "some random imagepath 1";
 	
 	@Before
 	public void setUp() throws Exception{
-		annotation = new Annotation(null,null);
+		annotation = new Annotation(author, comment);
+		annotation.addImagePath(imagepath1);
 	}
 	
+	/**
+	 * Test the setAuthor and setComment methods to make sure they are able to 
+	 * change the author and comment.
+	 */
 	@Test
-	public void test()
+	public void testChangeAuthorAndComment()
 	{
-		assertEquals(null,annotation.getComment() );
-		assertEquals(null,annotation.getAuthor());
-		//assertEquals("",annotations.getDescription );
 		annotation.setAuthor("Author");
 		annotation.setComment("comment");
 		//assertTrue(res);
 		assertEquals("Author",annotation.getComment() );
 		assertEquals("comment",annotation.getAuthor() );
 	}
+	
+	/**
+	 * Test the getAuthor() method to make sure it returns the author name.
+	 */
+	@Test
+	public void testGetAuthor() {
+		assertEquals(author, annotation.getAuthor());
+	}
+	
+	/**
+	 * Test the getComment() method to make sure it returns the comment.
+	 */
+	@Test
+	public void testGetComment() {
+		assertEquals(comment, annotation.getComment());
+	}
+	
+	/**
+	 * Test the methods that involve the imagepath.
+	 * Checks to see if adding and getting the imagepath from the arraylist of 
+	 * imagepaths works.
+	 */
+	@Test
+	public void testImagepath() {
+		String imagepath2 = "some imagepath 2";
+		
+		// add imagepath
+		annotation.addImagePath(imagepath2);
+		
+		// Test get imagepath. Position of imagepath2 should be at 1 because 
+		// there are only 2 elements in the arraylist (imagepath1 and imagepath2)
+		assertEquals(imagepath2, annotation.getImagePath(1));
+	}
+	
+
 
 }
