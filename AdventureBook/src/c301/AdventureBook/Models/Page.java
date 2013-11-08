c60b290269dcd54a2ff20168114e0568f310cd04
Justin Hoy <jjhoy@ualberta.ca>
2013-11-07 23:01:30 -0700

    Changes to Page class

---
+++ AdventureBook/src/c301/AdventureBook/Models/Page.java
@@ -0,0 +1,75 @@
+/* Copyright (C) <2013>  <Justin Hoy>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//Creator: Justin Hoy
+
+package c301.AdventureBook.Models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Page implements Serializable{
+
+	private String title;
+	private List<Option> options;
+	private String pageDescription;
+	
+	public Page(String title, String pageDescription){
+		this.title = title;
+		this.pageDescription = pageDescription;
+		this.options = new ArrayList<Option>();
+	}
+	
+	public void deleteOption(Option option) {
+		this.options.remove(option);
+	}
+	
+	public void addOption(Option option) {
+		this.options.add(option);
+	}
+
+	public void setTitle(String startPage){
+		this.title = startPage;
+	}
+	
+	public void setOptions(List<Option> options){
+		this.options = options;
+	}
+	
+	public void setPageDescription(String pageDescription) {
+		this.pageDescription =  pageDescription;
+	}
+	
+	public String getPageDescription() {
+		return pageDescription;
+	}
+	
+	public String getTitle(){
+		return this.title;
+	}
+	
+	public List<Option> getOptions(){
+		return this.options;
+	}
+	
+    @Override
+    public String toString() {
+        return this.title;
+    }
+    
+
+}
