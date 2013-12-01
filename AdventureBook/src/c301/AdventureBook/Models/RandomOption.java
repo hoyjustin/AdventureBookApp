d21997b061d10466a224f4125cef2575355024f3
tyleung <tyleung@ualberta.ca>
2013-11-30 19:49:43 -0700

    added documentation to all models

--- AdventureBook/src/c301/AdventureBook/Models/RandomOption.java
+++ AdventureBook/src/c301/AdventureBook/Models/RandomOption.java
@@ -1,9 +1,16 @@
 package c301.AdventureBook.Models;
 
-
+/**
+ * The Random Option Model Class.
+ * This class is used to define a random option on a page.
+ * 
+ */
 public class RandomOption extends Option{
 	
-	
+	/**
+	 * Constructor for the Random Option Class.
+	 * Creates a random option by calling super on its parent method.
+	 */
 	public RandomOption() {
 		super("I'm Feeling Lucky!", null);
 	}
